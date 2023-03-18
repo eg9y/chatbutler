de33d94c05cc6eb972a23b6086a2ac063a4b458b
Egan Bisma <brickkace@gmail.com>
2023-03-18 12:48:22 +0700

    change font to default since inconsistent weights in different browser engines

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,23 +4,31 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		fontFamily: {
-			sans: [
-				'ui-rounded',
-				"'Hiragino Maru Gothic ProN'",
-				'Quicksand',
-				'Comfortaa',
+		// fontFamily: {
+		// 	sans: [
+		// 		'ui-rounded',
+		// 		"'Hiragino Maru Gothic ProN'",
+		// 		'Quicksand',
+		// 		'Comfortaa',
 
-				'Manjari',
-				'Arial Rounded MT Bold',
-				'Calibri',
-				'source-sans-pro',
-				'sans-serif',
-			],
-		},
+		// 		'Manjari',
+		// 		'Arial Rounded MT Bold',
+		// 		'Calibri',
+		// 		'source-sans-pro',
+		// 		'sans-serif',
+		// 	],
+		// },
 		extend: {
 			borderWidth: {
 				1: '1px',
+			},
+			fontWeight: {
+				'extra-light': 200,
+				light: 300,
+				normal: 400,
+				medium: 500,
+				semibold: 600,
+				bold: 700,
 			},
 		},
 	},

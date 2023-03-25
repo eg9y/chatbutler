3f8bb7a2a5e6665b1802536c932cc9d8b4ce9108
Egan Bisma <brickkace@gmail.com>
2023-03-25 15:04:25 +0700

    change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,20 +4,19 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		// fontFamily: {
-		// 	sans: [
-		// 		'ui-rounded',
-		// 		"'Hiragino Maru Gothic ProN'",
-		// 		'Quicksand',
-		// 		'Comfortaa',
-
-		// 		'Manjari',
-		// 		'Arial Rounded MT Bold',
-		// 		'Calibri',
-		// 		'source-sans-pro',
-		// 		'sans-serif',
-		// 	],
-		// },
+		fontFamily: {
+			sans: [
+				'ui-rounded',
+				'Hiragino Maru Gothic ProN',
+				'Quicksand',
+				'Comfortaa',
+				'Manjari',
+				'Arial Rounded MT Bold',
+				'Calibri',
+				'source-sans-pro',
+				'sans-serif',
+			],
+		},
 		extend: {
 			borderWidth: {
 				1: '1px',

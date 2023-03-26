564aa1b5749b6ffc66c82fce27e7a83ae0f74ef5
Egan Bisma <brickkace@gmail.com>
2023-03-26 23:03:47 +0700

    env fix

--- src/auth/getUrl.tsx
+++ src/auth/getUrl.tsx
@@ -1,6 +1,6 @@
 const getURL = () => {
 	let url =
-		process.env.VERCEL_URL ?? // Automatically set by Vercel.
+		process.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
 		'http://localhost:3000/';
 	// Make sure to include `https://` when not localhost.
 	url = url.includes('http') ? url : `https://${url}`;

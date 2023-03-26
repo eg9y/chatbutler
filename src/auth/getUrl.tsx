ce7a6dabe6089b1c3892a0e8c16753942d6b59c2
Egan Bisma <brickkace@gmail.com>
2023-03-26 22:57:55 +0700

    correct envars

--- src/auth/getUrl.tsx
+++ src/auth/getUrl.tsx
@@ -1,6 +1,6 @@
 const getURL = () => {
 	let url =
-		import.meta.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+		import.meta.env.VERCEL_URL ?? // Automatically set by Vercel.
 		'http://localhost:3000/';
 	// Make sure to include `https://` when not localhost.
 	url = url.includes('http') ? url : `https://${url}`;

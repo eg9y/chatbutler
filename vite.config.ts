073e285742493c286d2fcba7ed4be6c9b8f801d7
Egan Bisma <brickkace@gmail.com>
2023-03-27 11:45:16 +0700

    vite multiupage app support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
@@ -21,4 +22,13 @@ export default defineConfig({
 			enforce: 'post',
 		},
 	],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				gallery: resolve(__dirname, 'src/pages/gallery.html'),
+				auth: resolve(__dirname, 'src/pages/auth.html'),
+			},
+		},
+	},
 });

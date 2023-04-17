2649429c424c0f14abebe7381ad121e9bf67377f
Egan Bisma <brickkace@gmail.com>
2023-04-17 13:32:25 +0700

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,38 @@
 export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
+	graphql_public: {
+		Tables: {
+			[_ in never]: never;
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			graphql: {
+				Args: {
+					operationName?: string;
+					query?: string;
+					variables?: Json;
+					extensions?: Json;
+				};
+				Returns: Json;
+			};
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
 	public: {
 		Tables: {
 			document_contents: {
 				Row: {
 					content: string | null;
 					document_id: number;
-					embedding: unknown | null;
+					embedding: string | null;
 					id: string;
 					token_count: number | null;
 					user_id: string;
@@ -15,15 +40,15 @@ export interface Database {
 				Insert: {
 					content?: string | null;
 					document_id: number;
-					embedding?: unknown | null;
-					id: string;
+					embedding?: string | null;
+					id?: string;
 					token_count?: number | null;
 					user_id: string;
 				};
 				Update: {
 					content?: string | null;
 					document_id?: number;
-					embedding?: unknown | null;
+					embedding?: string | null;
 					id?: string;
 					token_count?: number | null;
 					user_id?: string;
@@ -152,7 +177,7 @@ export interface Database {
 			};
 			match_document_contents: {
 				Args: {
-					query_embedding: unknown;
+					query_embedding: string;
 					match_count: number;
 					filter_document_ids?: number[];
 				};
@@ -161,6 +186,168 @@ export interface Database {
 					document_id: number;
 					content: string;
 					similarity: number;
+				}[];
+			};
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+	storage: {
+		Tables: {
+			buckets: {
+				Row: {
+					allowed_mime_types: string[] | null;
+					avif_autodetection: boolean | null;
+					created_at: string | null;
+					file_size_limit: number | null;
+					id: string;
+					name: string;
+					owner: string | null;
+					public: boolean | null;
+					updated_at: string | null;
+				};
+				Insert: {
+					allowed_mime_types?: string[] | null;
+					avif_autodetection?: boolean | null;
+					created_at?: string | null;
+					file_size_limit?: number | null;
+					id: string;
+					name: string;
+					owner?: string | null;
+					public?: boolean | null;
+					updated_at?: string | null;
+				};
+				Update: {
+					allowed_mime_types?: string[] | null;
+					avif_autodetection?: boolean | null;
+					created_at?: string | null;
+					file_size_limit?: number | null;
+					id?: string;
+					name?: string;
+					owner?: string | null;
+					public?: boolean | null;
+					updated_at?: string | null;
+				};
+			};
+			migrations: {
+				Row: {
+					executed_at: string | null;
+					hash: string;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					executed_at?: string | null;
+					hash: string;
+					id: number;
+					name: string;
+				};
+				Update: {
+					executed_at?: string | null;
+					hash?: string;
+					id?: number;
+					name?: string;
+				};
+			};
+			objects: {
+				Row: {
+					bucket_id: string | null;
+					created_at: string | null;
+					id: string;
+					last_accessed_at: string | null;
+					metadata: Json | null;
+					name: string | null;
+					owner: string | null;
+					path_tokens: string[] | null;
+					updated_at: string | null;
+					version: string | null;
+				};
+				Insert: {
+					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
+					name?: string | null;
+					owner?: string | null;
+					path_tokens?: string[] | null;
+					updated_at?: string | null;
+					version?: string | null;
+				};
+				Update: {
+					bucket_id?: string | null;
+					created_at?: string | null;
+					id?: string;
+					last_accessed_at?: string | null;
+					metadata?: Json | null;
+					name?: string | null;
+					owner?: string | null;
+					path_tokens?: string[] | null;
+					updated_at?: string | null;
+					version?: string | null;
+				};
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			can_insert_object: {
+				Args: {
+					bucketid: string;
+					name: string;
+					owner: string;
+					metadata: Json;
+				};
+				Returns: undefined;
+			};
+			extension: {
+				Args: {
+					name: string;
+				};
+				Returns: string;
+			};
+			filename: {
+				Args: {
+					name: string;
+				};
+				Returns: string;
+			};
+			foldername: {
+				Args: {
+					name: string;
+				};
+				Returns: unknown;
+			};
+			get_size_by_bucket: {
+				Args: Record<PropertyKey, never>;
+				Returns: {
+					size: number;
+					bucket_id: string;
+				}[];
+			};
+			search: {
+				Args: {
+					prefix: string;
+					bucketname: string;
+					limits?: number;
+					levels?: number;
+					offsets?: number;
+					search?: string;
+					sortcolumn?: string;
+					sortorder?: string;
+				};
+				Returns: {
+					name: string;
+					id: string;
+					updated_at: string;
+					created_at: string;
+					last_accessed_at: string;
+					metadata: Json;
 				}[];
 			};
 		};

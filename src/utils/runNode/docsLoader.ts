1852f0f5b1f7e83665da3cd82a53d8eebbf8f293
Egan Bisma <brickkace@gmail.com>
2023-05-18 19:52:40 +0700

    updated docsloader

--- src/utils/runNode/docsLoader.ts
+++ src/utils/runNode/docsLoader.ts
@@ -23,66 +23,18 @@ function pauser(node: CustomNode, get: () => RFState): Promise<string> {
 	});
 }
 
-const docsLoader = async (
-	node: Node<DocsLoaderDataType>,
-	get: () => RFState,
-	openAiKey: string,
-) => {
-	// try {
-	if (!node.data.response) {
+const docsLoader = async (node: Node<DocsLoaderDataType>, get: () => RFState) => {
+	if (node.data.askUser) {
+		console.log('ask user');
 		await pauser(node as CustomNode, get);
+	} else {
+		console.log('no ask user');
+		node.data.response = node.data.text;
 	}
-	// 	const docsLoaderNode = node.data as DocsLoaderDataType;
-
-	// 	if (docsLoaderNode.text.trim() === '') {
-	// 		throw new Error('No text provided');
-	// 	}
-	// 	const supabase = createSupabaseClient();
-
-	// 	let embeddings = new OpenAIEmbeddings({ openAIApiKey: openAiKey });
-	// 	const session = await supabase.auth.getSession();
-
-	// 	if (session.error) {
-	// 		throw new Error(session.error.message);
-	// 	}
-
-	// 	// if no sessions found, use api key set in non-user's "session"
-	// 	if (session && session.data && session.data.session && session.data.session.access_token) {
-	// 		embeddings = new OpenAIEmbeddings(
-	// 			{
-	// 				openAIApiKey: session.data.session.access_token,
-	// 			},
-	// 			{
-	// 				basePath: `${import.meta.env.VITE_SUPABASE_FUNCTION_URL}/openai`,
-	// 			},
-	// 		);
-	// 	}
-
-	// 	if (docsLoaderNode.source === DocSource.pdfUrl) {
-	// 		const loader = new PdfUrlLoader(docsLoaderNode.text);
-	// 		const docs = await loader.load();
-
-	// 		const splitter = new RecursiveCharacterTextSplitter();
-
-	// 		const docOutput = await splitter.splitDocuments([docs[0]]);
-	// 		console.log(docOutput);
-
-	// 		await SupabaseVectorStoreWithFilter.fromDocuments(docOutput, embeddings, {
-	// 			client: supabase,
-	// 		});
-	// 	}
-
-	// 	node.data = {
-	// 		...node.data,
-	// 		isLoading: false,
-	// 	};
-	// } catch (error) {
-	// 	node.data = {
-	// 		...node.data,
-	// 		isLoading: false,
-	// 	};
-	// 	throw error;
-	// }
+	node.data = {
+		...node.data,
+		isLoading: false,
+	};
 };
 
 export default docsLoader;
